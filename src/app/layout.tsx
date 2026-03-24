c6d78b66ae300233e725b871f9c4f54cc35fb806
amazkaleem <amazkaleem8@gmail.com>
2026-03-25 00:03:23 +0500

    Still Embedding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import Script from "next/script";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
       <head>
-        <Script
-          src="https://giving.gofundme.com/embedded/api/sdk/js/38471"
-          strategy="beforeInteractive"
-        />
+        <script async src="https://giving.gofundme.com/embedded/api/sdk/js/38471"></script>
       </head>
       <body>{children}</body>
     </html>
