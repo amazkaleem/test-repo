72e48025d7d6c3882a031d2b8a99a2ad043608d8
amazkaleem <amazkaleem8@gmail.com>
2026-03-25 13:41:33 +0500

    Reverting back to 'Inline Donation Grid'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
-import Script from "next/script";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <head />
-      <body>
-        {children}
-        <Script
-          src="https://giving.gofundme.com/embedded/api/sdk/js/38471"
-          strategy="afterInteractive"
-        />
-      </body>
+      <head>
+        <script async src="https://giving.gofundme.com/embedded/api/sdk/js/38471" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
